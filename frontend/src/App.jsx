f76555893de1dcf3d014bd9c2e3f1d3c7ce1294e
rolandserge <sergeroland.komenan@gmail.com>
2026-03-10 23:12:46 +0000

    creation de la branche develop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,12 +21,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        En voila des manieres ! anitche hein
       </p>
     </>
   )
